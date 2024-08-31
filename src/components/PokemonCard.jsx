bf47016750dd86d934bedaeeb719d0c1b8a9f8f9
pratham <158725920+SomethingOFF@users.noreply.github.com>
2024-08-31 23:36:22 +0530

    updated

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -3,6 +3,8 @@ const PokemonCard = ({ pokemon }) => {
         <div className="pokemon-card">
             <img src={pokemon.sprites.front_default} alt={pokemon.name} />
             <h3>{pokemon.name}</h3>
+            <h4>Moves: {pokemon.moves.length}</h4>
+            <div>weight: {pokemon.weight}</div>
         </div>
     )
 }
